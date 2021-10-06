5f0d7a18fcc8f33ad769e4a4d7ce91783c5020f5
admin <admin@MacBook-Pro-de-admin.local>
2021-10-06 15:04:16 +0200

    ajout du nom

--- blog_js/src/views/admin.vue
+++ blog_js/src/views/admin.vue
@@ -2,8 +2,13 @@
   <div id="app3">
     <label>Entrer votre titre : </label>
     <input type="text" id="titre" />
+
+    <label>Entrer votre nom : </label>
+    <input type="text" id="nom" />
+
     <label>Entrer votre texte : </label>
     <input type="text" id="input" />
+
     <button id="btnTache" @click="addTask()">add</button>
     <ol>
       <li v-for="(item, index) in contenu" :key="index">
@@ -25,12 +30,20 @@ export default {
     addTask() {
       let titre = document.getElementById("titre");
       let titleText = titre.value;
+
+      let nom = document.getElementById("nom");
+      let nameText = nom.value;
+
       let input = document.getElementById("input");
       let text = input.value;
-      let test = titleText + " " + text;
+
+      let test = titleText + " " + nameText + " " + text;
+
       this.contenu.push({ content: test, status: "To Do" });
+
       input.value = "";
       titre.value = "";
+      nom.value = "";
     },
     suppTask(index) {
       this.contenu.splice(index, 1);
