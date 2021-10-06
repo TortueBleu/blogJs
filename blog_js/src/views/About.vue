8a68493e9a9c789f7ac07c8c726a0c422552311f
Baptiste Choquet <baptiste.choquet@pm.me>
2021-10-06 11:01:03 +0200

    Cassoulet dans le about us

--- blog_js/src/views/About.vue
+++ blog_js/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      La légende, rapportée par Prosper Montagné2, place l'origine du cassoulet dans la ville de Castelnaudary, durant la guerre de Cent Ans (1337-1453). Durant le siège de Castelnaudary par les Anglais, les assiégés affamés auraient réuni tous les vivres disponibles (fèves et viandes), pour confectionner un gigantesque ragoût ou estofat, pour revigorer les combattants. Ceux-ci purent alors chasser les Anglais et libérer la ville.
+
+Cette légende, qui conforte le sentiment nationaliste et fait du cassoulet un défenseur des valeurs françaises, ne résiste cependant pas vraiment à l'analyse. Certes, la ville de Castelnaudary a grandement souffert pendant la guerre de Cent Ans. Elle fut d'ailleurs partiellement brûlée par les troupes du Prince Noir, le 31 octobre 1355. Cette légende reflète ainsi le désir de prendre une revanche sur l'Histoire. D'autre part, les haricots, originaires du continent sud-américain, n'ont été introduits en Europe que beaucoup plus tard (xvie siècle), il s'agit donc plus probablement de fèves, ou de doliques (moujette ou mounjette en occitan), qui étaient consommées à l'époque en ragoût.
+
+Un des livres de cuisine les plus anciens, Le Viandier, de Taillevent, édité le 15 février 1654 décrit une recette de ragoût de mouton aux fèves, probablement inspirée des Arabes3.
+
+Mais cette légende met en évidence la nature même du plat : un plat fait de restes et très énergétique. C'est donc à l'origine une recette familiale paysanne, faite de graines (fèves, doliques, puis plus tard, haricots blancs), dans lesquelles cuisent à petit feu les viandes disponibles dans le garde-manger des paysans de l'époque. Le plat est laissé sur un coin de la cuisinière tôt le matin, mijote ainsi pendant la journée et est consommé lors du repas du soir4.
+
+Le cassoulet, qui s'appelle encore au xviie siècle estouffet, prend au xviiie siècle, le nom de cassoulet du nom du plat dans lequel il cuit, la « cassole ». La querelle sur le pays d'origine du cassoulet apparaît, dès la fin du xixe siècle, quand La Revue méridionale publie, en 1890, un article affirmant que le seul cassoulet authentique vient de Castelnaudary5.
+
+Anatole France appuie cette version, quand il écrit « iI ne faut pas confondre le cassoulet de Castelnaudary avec celui de Carcassonne qui est un simple gigot de mouton aux haricots ». La question se nationalise, vers 1900, quand le critique gastronomique Edmond Richardin lance un débat sur ce sujet dans la gazette parisienne6. Chacun prend alors parti pour un pays (Castelnaudary, Carcassonne, Toulouse), en oubliant l'existence de versions locales comme Villefranche-de-Lauragais, Narbonne, Montauban, Pau ou Pamiers. En 1911, le cassoulet est chanté par le poète félibre, Auguste Fourès, qui retranscrit un chant anonyme de 18507. Ce plat s'invite sur la table de l'Élysée par le biais d'Armand Fallières6.
+
+De nos jours, on trouve dans la grande distribution des conserves de cassoulet dans une version que renient les puristes. Il existe cependant, dans la région, des versions gastronomiques appertisées tout à fait remarquables. Sa confection est très codifiée, mais varie selon les écoles.
+    </p>
   </div>
 </template>
