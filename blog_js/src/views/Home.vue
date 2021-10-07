0e7b52d75127520b6abd6a0f026a51178667c852
Léonard <leonard.duhem@gmail.com>
2021-10-07 10:26:08 +0200

    set up du store

--- blog_js/src/views/Home.vue
+++ blog_js/src/views/Home.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home container">
-    <div v-for="item in articles" :key="item" class="w-50 p-3 mx-auto mb-2 border border-secondary rounded shadow p-3 mb-5 bg-white rounded"> 
+    <div v-for="(item, index) in listArticles" :key="index" :item="item" class="w-50 p-3 mx-auto mb-2 border border-secondary rounded shadow p-3 mb-5 bg-white rounded"> 
         <h1>{{item.titre}}</h1>
         <h4>{{item.intro}}</h4>
-        <p>{{item.auteur}}</p>
+        <p>{{item.nom}}</p>
         <button class="btn btn-primary mb-2">lire Plus</button>
         <p>{{item.date}}</p>
     </div>  
@@ -17,22 +17,15 @@ export default {
   components: {
     
   },
+
+  computed: {
+    listArticles() {
+      return this.$store.state.articles
+    }
+  },
   data() {
       return{
-        articles :   [
-          {
-            titre : "Article 1",
-            intro : "test 1",
-            date : "06/10/2021",
-            auteur: "Tac"
-          } ,
-          {
-            titre : "Article 2",
-            intro : "test 2",
-            date : "07/10/2021",
-            auteur : "Tac2"
-          } ,
-        ] 
+        
       }     
     },
 };
